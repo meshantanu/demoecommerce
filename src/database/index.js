1f683116f7d3942650c5076eeee4793eefe4931e
Shantanu Mishra <shantanu.mishra@visionias.in>
2023-10-23 15:06:59 +0530

    commit the build for demo

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+    "mongodb+srv://shantanumishra:12341234@cluster0.wsa51pv.mongodb.net/";
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+export default connectToDB;
